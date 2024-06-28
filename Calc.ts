c66e02c7a08e49cc3122f57868b34abfa9985c17
Xoli-Nxiweni <120707547+Xoli-Nxiweni@users.noreply.github.com>
2024-06-28 08:24:36 +0200

    Update Calc.ts

--- Calc.ts
+++ Calc.ts
@@ -6,10 +6,4 @@ let calculate = new Calculator();
     Quotient = ${calculate.divide(400, 2)}
     Product = ${calculate.multiply(4, 16)}
     Difference = ${calculate.subtract(46, 6)}`);
-    
-let array : number [] = [2,64,4,100,5,57,6,7,9,10,3,87,1]
-let sortArray = (param:number[]) : number []=>{
-    let newArray : number [] = param.sort((first:number, second:number) => first - second)
-    return newArray
-}
-console.log(sortArray(array));
+
